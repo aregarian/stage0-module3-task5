f772fa0f0d1af661eabf182c1315d4052c02b4d2
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-18 19:05:41 +0300

    hehe boy112

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,13 +2,7 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
-        int first;
-        int second;
-        int third;
-
-        first = 1;
-        second = 10;
-        third = 100;
-        System.out.print(first + "" + second + "" + third);
+        int first = 1, second = 10, third = 100;
+        System.out.println(first + "" + second + "" + third);
     }
 }
